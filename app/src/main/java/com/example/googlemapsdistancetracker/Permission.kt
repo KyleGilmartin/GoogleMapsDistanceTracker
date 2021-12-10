ca66c68ab795a21c2fd4cdf1a880fd0016102d69
kyle-gilmartin <kylegilmartin8@gmail.com>
2021-12-10 15:58:33 +0000

    enabled datablinding and added in Permission.kt

---
+++ app/src/main/java/com/example/googlemapsdistancetracker/Permission.kt
@@ -0,0 +1,27 @@
+package com.example.googlemapsdistancetracker
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.example.googlemapsdistancetracker.Constants.PERMISSION_LOCATION_REQUEST_CODE
+import com.vmadalin.easypermissions.EasyPermissions
+import java.util.jar.Manifest
+
+object Permission {
+
+
+    fun hasLocationPermission(context: Context){
+        EasyPermissions.hasPermissions(
+            context,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    fun requestLocationPermission(fragment: Fragment){
+        EasyPermissions.requestPermissions(
+            fragment,
+            "This app can not work without permissions",
+            PERMISSION_LOCATION_REQUEST_CODE,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+}
